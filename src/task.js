acbb70cb779bb71c07864ef9d2a4fd6fe2220715
Greg Kikola <gkikola@gmail.com>
2021-11-23 18:49:50 -0500

    Simplify task factory function to only take title

--- src/task.js
+++ src/task.js
@@ -7,13 +7,9 @@ const taskProto = {
   completed: false,
 };
 
-function createTask(title, dueDate) {
-  if (dueDate && !dueDate instanceof Date)
-    throw new TypeError('Invalid datatype for due date: expected Date object');
-
+function createTask(title) {
   const task = Object.create(taskProto);
   task.title = title;
-  task.dueDate = dueDate;
 
   return task;
 }
