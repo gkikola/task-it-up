79557eec0cc7550ae8885fbbb985dcdff6e35751
Greg Kikola <gkikola@gmail.com>
2021-11-23 20:33:12 -0500

    Add high-level page layout

--- src/index.js
+++ src/index.js
@@ -1,6 +1,39 @@
+function createHeader() {
+  const header = document.createElement('header');
+  header.id = 'header';
+
+  return header;
+}
+
+function createSidePanel() {
+  const panel = document.createElement('aside');
+  panel.id = 'side-panel';
+
+  return panel;
+}
+
+function createMainPanel() {
+  const panel = document.createElement('main');
+  panel.id = 'main-panel';
+
+  return panel;
+}
+
+function createFooter() {
+  const footer = document.createElement('footer');
+  footer.id = 'footer';
+
+  return footer;
+}
+
 function createApp() {
   const container = document.createElement('div');
   container.id = 'app';
+
+  container.appendChild(createHeader());
+  container.appendChild(createSidePanel());
+  container.appendChild(createMainPanel());
+  container.appendChild(createFooter());
 
   return container;
 }
