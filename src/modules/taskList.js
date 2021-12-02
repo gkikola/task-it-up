849a9923057f111f64d9cc015efa0d9b98bd8737
Greg Kikola <gkikola@gmail.com>
2021-12-02 17:35:26 -0500

    Add TaskList method to add a task to the list

--- src/modules/taskList.js
+++ src/modules/taskList.js
@@ -1,5 +1,23 @@
+import _ from 'lodash';
+import { v4 as uuid } from 'uuid';
+
 class TaskList {
-  tasks = [];
+  #tasks;
+
+  constructor() {
+    this.#tasks = new Map();
+  }
+
+  addTask(task) {
+    // Generate UUID (loop just in case there's a collision)
+    let id;
+    do {
+      id = uuid();
+    } while (this.#tasks.has(id));
+
+    // Add task to task map
+    this.#tasks.set(id, _.cloneDeep(task));
+  }
 }
 
 export default TaskList;
