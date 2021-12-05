949c9cbf216e7e71ec2dc36f3ad5f3f9cb1773ee
Greg Kikola <gkikola@gmail.com>
2021-12-05 13:59:26 -0500

    Add FilterMenu methods to add and remove filters

--- src/modules/filterMenu.js
+++ src/modules/filterMenu.js
@@ -16,7 +16,9 @@ class FilterMenu {
    *   menu.
    * @param {Object[]} [groups] An array of filter groups to add to the menu.
    * @param {string} groups[].id The identifier of the filter group.
-   * @param {string} groups[].label The displayed label for the filter group.
+   * @param {string} [groups[].label] The displayed label for the filter group.
+   *   If not given, the group will have no heading and will not be
+   *   collapsible.
    */
   constructor(parent, groups) {
     /**
@@ -26,30 +28,8 @@ class FilterMenu {
     this._container = document.createElement('div');
     this._container.classList.add('filter-menu');
 
-    /**
-     * A task filter.
-     * @typedef {Object} module:filterMenu~FilterMenu~filter
-     * @property {string} id The identifier of the filter. This ID should be
-     *   unique among filters belonging to the same group, but not necessarily
-     *   among all filters.
-     * @property {string} label The displayed label for the filter.
-     */
-
-    /**
-     * A group of task filters.
-     * @typedef {Object} module:filterMenu~FilterMenu~filterGroup
-     * @property {string} id The unique identifier of the group.
-     * @property {string} label The displayed label for the group.
-     * @property {filter[]} filters The array of task filters.
-     * @property {boolean} expanded Indicates whether the group is expanded
-     *   (its filters visible) or collapsed (its filters hidden).
-     */
-
-    /**
-     * A list of filter groups.
-     * @type {module:filterMenu~FilterMenu~filterGroup[]}
-     */
-    this._groups = [];
+    if (groups)
+      groups.forEach(group => this.addGroup(group.id, group.label));
 
     parent.appendChild(this._container);
   }
@@ -61,10 +41,9 @@ class FilterMenu {
    *   the group will have no heading and will not be collapsible.
    */
   addGroup(id, label) {
-    this._groups.push({ id, label, filters: [] });
-
     const groupContainer = document.createElement('div');
     groupContainer.classList.add('filter-group');
+    groupContainer.classList.add('expanded');
     groupContainer.dataset.groupId = id;
 
     if (label) {
@@ -98,6 +77,66 @@ class FilterMenu {
     groupContainer.appendChild(list);
 
     this._container.appendChild(groupContainer);
+  }
+
+  /**
+   * Add a filter to the menu.
+   * @param {string} groupId The identifier of the group in which to insert the
+   *   filter.
+   * @param {string} filterId The identifier for the filter.
+   * @param {string} label The displayed label for the filter.
+   * @param {number} [count=0] The number of tasks matching the filter.
+   */
+  addFilter(groupId, filterId, label, count = 0) {
+    const selector = `.filter-group[data-group-id="${groupId}"] .filter-list`;
+    const list = this._container.querySelector(selector);
+
+    if (!list)
+      throw new RangeError(`Could not locate filter group "${groupId}"`);
+
+    const item = document.createElement('li');
+    item.classList.add('filter-item');
+    item.dataset.filterId = filterId;
+
+    const labelElem = document.createElement('div');
+    labelElem.classList.add('filter-item-label');
+    labelElem.textContent = label;
+    item.appendChild(labelElem);
+
+    const countElem = document.createElement('div');
+    countElem.classList.add('filter-item-count');
+    countElem.textContent = (count > 0) ? count : '';
+    item.appendChild(countElem);
+
+    list.appendChild(item);
+  }
+
+  /**
+   * Remove a filter from the menu.
+   * @param {string} groupId The identifier of the group containing the filter.
+   * @param {string} filterId The identifier of the filter to remove.
+   */
+  removeFilter(groupId, filterId) {
+    const item = this._getFilterElement(groupId, filterId);
+    item.parentNode.removeChild(item);
+  }
+
+  /**
+   * Retrieve the list item element for a given filter.
+   * @param {string} groupId The identifier of the group containing the filter.
+   * @param {string} filterId The identifier of the filter.
+   * @returns {HTMLElement} The filter list item.
+   */
+  _getFilterElement(groupId, filterId) {
+    const selector = `.filter-group[data-group-id="${groupId}"] `
+      + `.filter-item[data-filter-id="${filterId}"]`;
+    const element = this._container.querySelector(selector);
+
+    if (!element)
+      throw new RangeError(`Could not locate filter "${filterId}" `
+        + `in group "${groupId}"`);
+
+    return element;
   }
 }
 
