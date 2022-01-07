5684a064a43e927c824b8799e5ed4a1a254467c7
Greg Kikola <gkikola@gmail.com>
2022-01-06 20:51:31 -0500

    Add lookup maps to TaskList

--- src/modules/taskList.js
+++ src/modules/taskList.js
@@ -3,8 +3,18 @@
  * @module taskList
  */
 
+import {
+  addToMapArray,
+  findInMapArray,
+  formatDate,
+  removeFromMapArrayBy,
+} from './utility';
+
 import _ from 'lodash';
 import { v4 as uuid } from 'uuid';
+import { format } from 'date-fns';
+
+const ISO_FORMAT = 'yyyy-MM-dd';
 
 /**
  * Container holding a list of tasks.
@@ -26,6 +36,32 @@ class TaskList {
      * @type {Map}
      */
     this._tasks = new Map();
+
+    /**
+     * Holds a map associating ISO date strings to arrays of
+     * [taskWrapper]{@link module:taskList~TaskList~taskWrapper} objects based
+     * on the due dates of the tasks. Tasks with no due date are assigned to
+     * the array associated with the string 'none'.
+     * @type {Map}
+     */
+    this._tasksByDueDate = new Map();
+
+    /**
+     * Holds a map associating project identifiers to arrays of
+     * [taskWrapper]{@link module:taskList~TaskList~taskWrapper} objects based
+     * on the projects associated with the tasks. Tasks not belonging to any
+     * project are assigned to the array associated with the string 'none'.
+     * @type {Map}
+     */
+    this._tasksByProject = new Map();
+
+    /**
+     * Holds a map associating priority numbers to arrays of
+     * [taskWrapper]{@link module:taskList~TaskList~taskWrapper} objects based
+     * on the priorities of the tasks.
+     * @type {Map}
+     */
+    this._tasksByPriority = new Map();
   }
 
   /**
@@ -43,7 +79,20 @@ class TaskList {
     } while (this._tasks.has(id));
 
     // Add task to task map
-    this._tasks.set(id, _.cloneDeep(task));
+    const copy = _.cloneDeep(task);
+    this._tasks.set(id, copy);
+
+    // Add task to lookup maps
+    const wrapper = { id, task: copy };
+    let dateStr = 'none';
+    if (task.dueDate)
+      dateStr = formatDate(task.dueDate, ISO_FORMAT);
+    const projectStr = task.project || 'none';
+    const priority = task.priority;
+    addToMapArray(this._tasksByDueDate, dateStr, Object.assign({}, wrapper));
+    addToMapArray(this._tasksByProject, projectStr, Object.assign({}, wrapper));
+    addToMapArray(this._tasksByPriority, priority, Object.assign({}, wrapper));
+
     return id;
   }
 
@@ -72,10 +121,38 @@ class TaskList {
    *   the given id is invalid, returns false.
    */
   updateTask(id, task) {
-    if (!this._tasks.has(id))
+    const oldTask = this._tasks.get(id);
+    if (!oldTask)
       return false;
 
-    this._tasks.set(id, _.cloneDeep(task));
+    const copy = _.cloneDeep(task);
+    this._tasks.set(id, copy);
+
+    const updateIndex = (map, oldKey, newKey) => {
+      if (oldKey === newKey) {
+        const entry = findInMapArray(map, newKey, elem => elem.id === id);
+        entry.task = copy;
+      } else {
+        removeFromMapArrayBy(map, oldKey, elem => elem.id === id);
+        addToMapArray(map, newKey, { id, task: copy });
+      }
+    };
+
+    let oldKey = 'none', newKey = 'none';
+    if (oldTask.dueDate)
+      oldKey = formatDate(oldTask.dueDate, ISO_FORMAT);
+    if (copy.dueDate)
+      newKey = formatDate(copy.dueDate, ISO_FORMAT);
+    updateIndex(this._tasksByDueDate, oldKey, newKey);
+
+    oldKey = oldTask.project || 'none';
+    newKey = copy.project || 'none';
+    updateIndex(this._tasksByProject, oldKey, newKey);
+
+    oldKey = oldTask.priority;
+    newKey = copy.priority;
+    updateIndex(this._tasksByPriority, oldKey, newKey);
+
     return true;
   }
 
