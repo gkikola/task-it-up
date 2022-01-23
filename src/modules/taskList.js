256253ae179f9ea2055c2000fab66c2b9c36531d
Greg Kikola <gkikola@gmail.com>
2022-01-23 13:05:30 -0500

    Fix linter errors in taskList module

--- src/modules/taskList.js
+++ src/modules/taskList.js
@@ -3,6 +3,10 @@
  * @module taskList
  */
 
+import { isBefore as isDateBefore, isSameDay } from 'date-fns';
+import _ from 'lodash';
+import { v4 as uuid } from 'uuid';
+
 import {
   addToMapArray,
   findInMapArray,
@@ -10,14 +14,33 @@ import {
   removeFromMapArrayBy,
 } from './utility';
 
-import {
-  isBefore as isDateBefore,
-  isSameDay,
-} from 'date-fns';
-import _ from 'lodash';
-import { v4 as uuid } from 'uuid';
-
 const ISO_FORMAT = 'yyyy-MM-dd';
+
+/**
+ * Object holding private members for the
+ * [TaskList]{@link module:taskList~TaskList} class.
+ * @typedef {Object} module:taskList~TaskList~privates
+ * @property {Map} tasks Holds a map associating UUIDs to tasks.
+ * @property {Map} tasksByDueDate Holds a map associating ISO date strings to
+ *   arrays of [taskWrapper]{@link module:taskList~TaskList~taskWrapper}
+ *   objects based on the due dates of the tasks. Tasks with no due date are
+ *   assigned to the array associated with the string 'none'.
+ * @property {Map} tasksByProject Holds a map associating project identifiers
+ *   to arrays of [taskWrapper]{@link module:taskList~TaskList~taskWrapper}
+ *   objects based on the projects associated with the tasks. Tasks not
+ *   belonging to any project are assigned to the array associated with the
+ *   string 'none'.
+ * @property {Map} tasksByPriority Holds a map associating priority numbers to
+ *   arrays of [taskWrapper]{@link module:taskList~TaskList~taskWrapper}
+ *   objects based on the priorities of the tasks.
+ */
+
+/**
+ * Holds private data for the [TaskList]{@link module:taskList~TaskList} class.
+ * @type {WeakMap}
+ * @see module:taskList~TaskList~privates
+ */
+const privateMembers = new WeakMap();
 
 /**
  * Container holding a list of tasks.
@@ -49,37 +72,13 @@ class TaskList {
    * Create a task list.
    */
   constructor() {
-    /**
-     * Holds a map associating UUIDs to tasks.
-     * @type {Map}
-     */
-    this._tasks = new Map();
-
-    /**
-     * Holds a map associating ISO date strings to arrays of
-     * [taskWrapper]{@link module:taskList~TaskList~taskWrapper} objects based
-     * on the due dates of the tasks. Tasks with no due date are assigned to
-     * the array associated with the string 'none'.
-     * @type {Map}
-     */
-    this._tasksByDueDate = new Map();
-
-    /**
-     * Holds a map associating project identifiers to arrays of
-     * [taskWrapper]{@link module:taskList~TaskList~taskWrapper} objects based
-     * on the projects associated with the tasks. Tasks not belonging to any
-     * project are assigned to the array associated with the string 'none'.
-     * @type {Map}
-     */
-    this._tasksByProject = new Map();
-
-    /**
-     * Holds a map associating priority numbers to arrays of
-     * [taskWrapper]{@link module:taskList~TaskList~taskWrapper} objects based
-     * on the priorities of the tasks.
-     * @type {Map}
-     */
-    this._tasksByPriority = new Map();
+    const privates = {
+      tasks: new Map(),
+      tasksByDueDate: new Map(),
+      tasksByProject: new Map(),
+      tasksByPriority: new Map(),
+    };
+    privateMembers.set(this, privates);
   }
 
   /**
@@ -90,26 +89,39 @@ class TaskList {
    * @returns {string} The identifier of the newly-added task.
    */
   addTask(task) {
+    const privates = privateMembers.get(this);
+
     // Generate UUID (loop just in case there's a collision)
     let id;
     do {
       id = uuid();
-    } while (this._tasks.has(id));
+    } while (privates.tasks.has(id));
 
     // Add task to task map
     const copy = _.cloneDeep(task);
-    this._tasks.set(id, copy);
+    privates.tasks.set(id, copy);
 
     // Add task to lookup maps
     const wrapper = { id, task: copy };
     let dateStr = 'none';
-    if (task.dueDate)
-      dateStr = formatDate(task.dueDate, ISO_FORMAT);
+    if (task.dueDate) dateStr = formatDate(task.dueDate, ISO_FORMAT);
     const projectStr = task.project || 'none';
-    const priority = task.priority;
-    addToMapArray(this._tasksByDueDate, dateStr, Object.assign({}, wrapper));
-    addToMapArray(this._tasksByProject, projectStr, Object.assign({}, wrapper));
-    addToMapArray(this._tasksByPriority, priority, Object.assign({}, wrapper));
+    const { priority } = task;
+    addToMapArray(
+      privates.tasksByDueDate,
+      dateStr,
+      { ...wrapper },
+    );
+    addToMapArray(
+      privates.tasksByProject,
+      projectStr,
+      { ...wrapper },
+    );
+    addToMapArray(
+      privates.tasksByPriority,
+      priority,
+      { ...wrapper },
+    );
 
     return id;
   }
@@ -123,10 +135,8 @@ class TaskList {
    *   not be found.
    */
   getTask(id) {
-    const task = this._tasks.get(id);
-    if (task)
-      return _.cloneDeep(task);
-
+    const task = privateMembers.get(this).tasks.get(id);
+    if (task) return _.cloneDeep(task);
     return undefined;
   }
 
@@ -139,37 +149,36 @@ class TaskList {
    *   the given id is invalid, returns false.
    */
   updateTask(id, task) {
-    const oldTask = this._tasks.get(id);
-    if (!oldTask)
-      return false;
+    const privates = privateMembers.get(this);
+    const oldTask = privates.tasks.get(id);
+    if (!oldTask) return false;
 
     const copy = _.cloneDeep(task);
-    this._tasks.set(id, copy);
+    privates.tasks.set(id, copy);
 
     const updateIndex = (map, oldKey, newKey) => {
       if (oldKey === newKey) {
-        const entry = findInMapArray(map, newKey, elem => elem.id === id);
+        const entry = findInMapArray(map, newKey, (elem) => elem.id === id);
         entry.task = copy;
       } else {
-        removeFromMapArrayBy(map, oldKey, elem => elem.id === id);
+        removeFromMapArrayBy(map, oldKey, (elem) => elem.id === id);
         addToMapArray(map, newKey, { id, task: copy });
       }
     };
 
-    let oldKey = 'none', newKey = 'none';
-    if (oldTask.dueDate)
-      oldKey = formatDate(oldTask.dueDate, ISO_FORMAT);
-    if (copy.dueDate)
-      newKey = formatDate(copy.dueDate, ISO_FORMAT);
-    updateIndex(this._tasksByDueDate, oldKey, newKey);
+    let oldKey = 'none';
+    let newKey = 'none';
+    if (oldTask.dueDate) oldKey = formatDate(oldTask.dueDate, ISO_FORMAT);
+    if (copy.dueDate) newKey = formatDate(copy.dueDate, ISO_FORMAT);
+    updateIndex(privates.tasksByDueDate, oldKey, newKey);
 
     oldKey = oldTask.project || 'none';
     newKey = copy.project || 'none';
-    updateIndex(this._tasksByProject, oldKey, newKey);
+    updateIndex(privates.tasksByProject, oldKey, newKey);
 
     oldKey = oldTask.priority;
     newKey = copy.priority;
-    updateIndex(this._tasksByPriority, oldKey, newKey);
+    updateIndex(privates.tasksByPriority, oldKey, newKey);
 
     return true;
   }
@@ -181,39 +190,27 @@ class TaskList {
    *   successfully. Otherwise, if the given id was not found, returns false.
    */
   deleteTask(id) {
-    const task = this._tasks.get(id);
-
-    if (!task)
-      return false;
+    const privates = privateMembers.get(this);
+    const task = privates.tasks.get(id);
+    if (!task) return false;
 
     let dateStr = 'none';
-    if (task.dueDate)
-      dateStr = formatDate(task.dueDate, ISO_FORMAT);
+    if (task.dueDate) dateStr = formatDate(task.dueDate, ISO_FORMAT);
     const projectStr = task.project || 'none';
-    const priority = task.priority;
+    const { priority } = task;
 
-    this._tasks.delete(id);
-    removeFromMapArrayBy(this._tasksByDueDate, dateStr, elem => {
-      return elem.id === id;
-    });
-    removeFromMapArrayBy(this._tasksByProject, projectStr, elem => {
-      return elem.id === id;
-    });
-    removeFromMapArrayBy(this._tasksByPriority, priority, elem => {
-      return elem.id === id;
-    });
+    privates.tasks.delete(id);
+    removeFromMapArrayBy(privates.tasksByDueDate, dateStr, (elem) => (
+      elem.id === id
+    ));
+    removeFromMapArrayBy(privates.tasksByProject, projectStr, (elem) => (
+      elem.id === id
+    ));
+    removeFromMapArrayBy(privates.tasksByPriority, priority, (elem) => (
+      elem.id === id
+    ));
 
     return true;
-  }
-
-  /**
-   * Iterate over the task list. Each iteration yields a wrapper containing the
-   * identifier of the task along with the task itself.
-   * @yields {module:taskList~TaskList~taskWrapper} The next task in the list.
-   */
-  *[Symbol.iterator]() {
-    for (const entry of this._tasks)
-      yield { id: entry[0], task: _.cloneDeep(entry[1]) };
   }
 
   /**
@@ -246,6 +243,7 @@ class TaskList {
    *   [taskWrapper]{@link module:taskList~TaskList~taskWrapper} objects.
    */
   entries(options = {}) {
+    const privates = privateMembers.get(this);
     const sortBy = options.sortBy || [];
 
     // Which index to use: default | due-date | project | priority
@@ -261,68 +259,77 @@ class TaskList {
     let output = [];
     const copyTasks = (map, key) => {
       const tasks = map.get(key);
-      if (tasks)
-        tasks.forEach(task => output.push(_.cloneDeep(task)));
+      if (tasks) tasks.forEach((task) => output.push(_.cloneDeep(task)));
     };
     switch (lookupType) {
-      default:
-      case 'default':
-        output = [...this];
-        break;
       case 'due-date': {
-        const dates = [...this._tasksByDueDate.keys()];
+        const dates = [...privates.tasksByDueDate.keys()];
         dates.sort();
 
-        let startKey = null, endKey = null;
-        if (options.startDate)
+        let startKey = null;
+        let endKey = null;
+        if (options.startDate) {
           startKey = formatDate(options.startDate, ISO_FORMAT);
-        if (options.endDate)
+        }
+        if (options.endDate) {
           endKey = formatDate(options.endDate, ISO_FORMAT);
+        }
 
-        let lowIndex = 0, highIndex = dates.length;
-        if (startKey)
+        let lowIndex = 0;
+        let highIndex = dates.length;
+        if (startKey) {
           lowIndex = _.sortedIndex(dates, startKey);
-        if (endKey)
+        }
+        if (endKey) {
           highIndex = _.sortedLastIndex(dates, endKey);
-        else if (dates.length > 0 && dates[dates.length - 1] === 'none')
-          highIndex--;
+        } else if (dates.length > 0 && dates[dates.length - 1] === 'none') {
+          highIndex -= 1;
+        }
 
-        dates.slice(lowIndex, highIndex).forEach(key => {
-          copyTasks(this._tasksByDueDate, key);
+        dates.slice(lowIndex, highIndex).forEach((key) => {
+          copyTasks(privates.tasksByDueDate, key);
         });
-        copyTasks(this._tasksByDueDate, 'none');
+        copyTasks(privates.tasksByDueDate, 'none');
         break;
       }
       case 'priority': {
-        copyTasks(this._tasksByPriority, options.priority);
+        copyTasks(privates.tasksByPriority, options.priority);
         break;
       }
       case 'project':
-        copyTasks(this._tasksByProject, options.project);
+        copyTasks(privates.tasksByProject, options.project);
+        break;
+      case 'default':
+      default:
+        privates.tasks.forEach((task, id) => {
+          output.push({ id, task: _.cloneDeep(task) });
+        });
         break;
     }
 
-    output = output.filter(entry => {
-      const task = entry.task;
+    output = output.filter((entry) => {
+      const { task } = entry;
       if (task.dueDate) {
-        if (options.startDate && isDateBefore(task.dueDate, options.startDate))
+        if (options.startDate
+          && isDateBefore(task.dueDate, options.startDate)) {
           return false;
-        if (options.endDate && isDateBefore(options.endDate, task.dueDate))
+        }
+        if (options.endDate && isDateBefore(options.endDate, task.dueDate)) {
           return false;
+        }
       }
-      if (!options.completed && task.completionDate)
-        return false;
-      if (options.requireDueDate && !task.dueDate)
-        return false;
+      if (!options.completed && task.completionDate) return false;
+      if (options.requireDueDate && !task.dueDate) return false;
       if (options.project) {
-        if (options.project === 'none' && task.project)
+        if (options.project === 'none' && task.project) return false;
+        if (options.project !== 'none' && task.project !== options.project) {
           return false;
-        if (options.project !== 'none' && task.project !== options.project)
-          return false;
+        }
       }
       if (typeof options.priority === 'number'
-        && task.priority !== options.priority)
+        && task.priority !== options.priority) {
         return false;
+      }
 
       return true;
     });
@@ -330,15 +337,13 @@ class TaskList {
     output = output.sort((a, b) => {
       const leftTask = a.task;
       const rightTask = b.task;
-      for (let index = 0; index < sortBy.length; index++) {
+      for (let index = 0; index < sortBy.length; index += 1) {
         const caseSensitive = sortBy[index].caseSensitive || false;
         const descending = sortBy[index].descending || false;
         const missingLast = sortBy[index].missingLast || false;
         const LESS = descending ? 1 : -1;
         const MORE = descending ? -1 : 1;
         switch (sortBy[index].field) {
-          default:
-            return 0;
           case 'name': {
             let leftName = leftTask.name;
             let rightName = rightTask.name;
@@ -346,59 +351,54 @@ class TaskList {
               leftName = leftName.toLowerCase();
               rightName = rightName.toLowerCase();
             }
-            if (leftName < rightName)
-              return LESS;
-            else if (leftName > rightName)
-              return MORE;
+            if (leftName < rightName) return LESS;
+            if (leftName > rightName) return MORE;
             break;
           }
           case 'due-date': {
             const leftDate = leftTask.dueDate;
             const rightDate = rightTask.dueDate;
-            if (!leftDate && rightDate)
-              return missingLast ? MORE : LESS;
-            if (leftDate && !rightDate)
-              return missingLast ? LESS : MORE;
-            if (leftDate && rightDate && !isSameDay(leftDate, rightDate))
+            if (!leftDate && rightDate) return missingLast ? MORE : LESS;
+            if (leftDate && !rightDate) return missingLast ? LESS : MORE;
+            if (leftDate && rightDate && !isSameDay(leftDate, rightDate)) {
               return isDateBefore(leftDate, rightDate) ? LESS : MORE;
+            }
             break;
           }
           case 'create-date':
-            if (isDateBefore(leftTask.creationDate, rightTask.creationDate))
+            if (isDateBefore(leftTask.creationDate, rightTask.creationDate)) {
               return LESS;
-            if (isDateBefore(rightTask.creationDate, leftTask.creationDate))
+            }
+            if (isDateBefore(rightTask.creationDate, leftTask.creationDate)) {
               return MORE;
+            }
             break;
           case 'priority':
-            if (leftTask.priority > rightTask.priority)
-              return LESS;
-            if (leftTask.priority < rightTask.priority)
-              return MORE;
+            if (leftTask.priority > rightTask.priority) return LESS;
+            if (leftTask.priority < rightTask.priority) return MORE;
             break;
           case 'project': {
             const leftProj = leftTask.project;
             const rightProj = rightTask.project;
-            if (!leftProj && rightProj)
-              return missingLast ? MORE : LESS;
-            if (leftProj && !rightProj)
-              return missingLast ? LESS : MORE;
+            if (!leftProj && rightProj) return missingLast ? MORE : LESS;
+            if (leftProj && !rightProj) return missingLast ? LESS : MORE;
             if (leftProj && rightProj) {
-              const projectList = options.projectList;
-              if (!projectList)
-                break;
+              const { projectList } = options;
+              if (!projectList) break;
+
               let leftName = projectList.getProject(leftProj).name;
               let rightName = projectList.getProject(rightProj).name;
               if (!caseSensitive) {
                 leftName = leftName.toLowerCase();
                 rightName = rightName.toLowerCase();
               }
-              if (leftName < rightName)
-                return LESS;
-              else if (leftName > rightName)
-                return MORE;
+              if (leftName < rightName) return LESS;
+              if (leftName > rightName) return MORE;
             }
             break;
           }
+          default:
+            return 0;
         }
       }
 
