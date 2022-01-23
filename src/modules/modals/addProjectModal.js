f3c6e361c9d492dd6974bd2275b4216de43fb34e
Greg Kikola <gkikola@gmail.com>
2022-01-23 13:22:16 -0500

    Fix linter errors in addProjectModal module

--- src/modules/modals/addProjectModal.js
+++ src/modules/modals/addProjectModal.js
@@ -8,6 +8,51 @@ import Project from '../project';
 import { createFormControl } from '../utility';
 
 /**
+ * Object holding private members for the
+ * [AddProjectModal]{@link module:addProjectModal~AddProjectModal} class.
+ * @typedef {Object} module:addProjectModal~AddProjectModal~privates
+ * @property {module:project~Project} [project] The project being edited, if
+ *   any.
+ * @property {string} mode Indicates the status of the project being entered.
+ *   If set to 'add', a new project is being created, and if set to 'edit',
+ *   then an existing project is being updated.
+ * @property {Object} callbacks An object holding callback functions.
+ * @property {Function} [callbacks.confirm] A callback function that will be
+ *   invoked when the user successfully confirms the modal.
+ * @property {Function} [callbacks.cancel] A callback function that will be
+ *   invoked when the user cancels the modal.
+ * @property {Object} controls An object holding the form input elements for
+ *   the modal.
+ * @property {HTMLElement} controls.name The text input element for the project
+ *   name.
+ * @property {HTMLElement} controls.description The textarea element for the
+ *   project description.
+ */
+
+/**
+ * Holds private data for the
+ * [AddProjectModal]{@link module:addProjectModal~AddProjectModal} class.
+ * @type {WeakMap}
+ * @see module:addProjectModal~AddProjectModal~privates
+ */
+const privateMembers = new WeakMap();
+
+/**
+ * Initialize the values of the form elements based on the initial project that
+ * was passed to the constructor, if any.
+ * @param {module:addProjectModal~AddProjectModal} instance The class instance
+ *   on which to apply the function.
+ */
+function initFormValues(instance) {
+  const { project, controls } = privateMembers.get(instance);
+
+  if (project) {
+    controls.name.value = project.name || '';
+    controls.description.value = project.description || '';
+  }
+}
+
+/**
  * A modal dialog for adding or editing a project.
  * @implements {module:modalStack~Modal}
  */
@@ -30,62 +75,38 @@ class AddProjectModal {
    *   object holding configuration options for the modal.
    */
   constructor(options = {}) {
-    /**
-     * The project being edited, if any.
-     * @type {module:project~Project}
-     */
-    this._project = options.project || null;
-
-    /**
-     * Indicates the status of the project being entered. If set to 'add', a
-     * new project is being created, and if set to 'edit', then an existing
-     * project is being updated.
-     * @type {string}
-     */
-    this._mode = options.project ? 'edit' : 'add';
-
-    /**
-     * An object holding callback functions.
-     * @type {Object}
-     * @property {Function} [confirm] A callback function that will be invoked
-     *   when the user successfully confirms the modal.
-     * @property {Function} [cancel] A callback function that will be invoked
-     *   when the user cancels the modal.
-     */
-    this._callbacks = {
-      confirm: options.confirm || null,
-      cancel: options.cancel || null,
-    }
-
-    /**
-     * An object holding the form input elements for the modal.
-     * @type {Object}
-     * @property {HTMLElement} name The text input element for the project
-     *   name.
-     * @property {HTMLElement} description The textarea element for the project
-     *   description.
-     */
-    this._controls = {
-      name: null,
-      description: null,
+    const privates = {
+      project: options.project || null,
+      mode: options.project ? 'edit' : 'add',
+      callbacks: {
+        confirm: options.confirm || null,
+        cancel: options.cancel || null,
+      },
+      controls: {
+        name: null,
+        description: null,
+      },
     };
+    privateMembers.set(this, privates);
   }
 
   get title() {
-    return (this._mode === 'edit') ? 'Edit Project' : 'Add Project';
+    const privates = privateMembers.get(this);
+    return (privates.mode === 'edit') ? 'Edit Project' : 'Add Project';
   }
 
   get confirmLabel() {
-    return (this._mode === 'edit') ? 'Update' : 'Add';
+    const privates = privateMembers.get(this);
+    return (privates.mode === 'edit') ? 'Update' : 'Add';
   }
 
   get initFocus() {
-    return this._controls.name;
+    return privateMembers.get(this).controls.name;
   }
 
   addContent(parent) {
     const containerType = { classList: ['form-input-container'] };
-    const labelType = value => ({ value, classList: ['form-input-label'] });
+    const labelType = (value) => ({ value, classList: ['form-input-label'] });
     parent.appendChild(createFormControl({
       type: 'text',
       id: 'project-name',
@@ -106,47 +127,32 @@ class AddProjectModal {
       size: { rows: 4, cols: 20 },
     }));
 
-    this._controls = {
+    privateMembers.get(this).controls = {
       name: parent.querySelector('#project-name'),
       description: parent.querySelector('#project-description'),
-    }
+    };
 
-    this._initFormValues();
+    initFormValues(this);
   }
 
   confirm() {
-    const controls = this._controls;
+    const privates = privateMembers.get(this);
+    const { controls } = privates;
     const project = new Project(controls.name.value, {
       description: controls.description.value || null,
     });
 
-    if (this._callbacks.confirm)
-      this._callbacks.confirm(project);
+    if (privates.callbacks.confirm) privates.callbacks.confirm(project);
   }
 
   cancel() {
-    if (this._callbacks.cancel)
-      this._callbacks.cancel();
+    const privates = privateMembers.get(this);
+    if (privates.callbacks.cancel) privates.callbacks.cancel();
   }
 
   validate() {
-    if (!this._controls.name.reportValidity())
-      return false;
+    if (!privateMembers.get(this).controls.name.reportValidity()) return false;
     return true;
-  }
-
-  /**
-   * Initialize the values of the form elements based on the initial project
-   * that was passed to the constructor, if any.
-   */
-  _initFormValues() {
-    const project = this._project;
-    const controls = this._controls;
-
-    if (project) {
-      controls.name.value = project.name || '';
-      controls.description.value = project.description || '';
-    }
   }
 }
 
