a08df15f50129db382eed21fc5ce1fccf6b41a45
Greg Kikola <gkikola@gmail.com>
2022-01-23 18:41:52 -0500

    Set up eslint-plugin-compat

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,20 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true,
   },
   extends: [
     'airbnb-base',
+    'plugin:compat/recommended'
   ],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
   rules: {
+  },
+  settings: {
+    polyfills: [
+      'Number.isFinite',
+    ],
   },
 };
