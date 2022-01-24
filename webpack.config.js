65cb3019dbc7dc7bfdb03d2cddb921dbc45dd315
Greg Kikola <gkikola@gmail.com>
2022-01-23 20:11:27 -0500

    Build production by default

--- webpack.config.js
+++ webpack.config.js
@@ -1,52 +1,65 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-  entry: './src/index.js',
-  mode: 'development',
-  devtool: 'inline-source-map',
-  devServer: {
-    static: './dist',
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/template.ejs',
-      title: 'Task It Up',
-    }),
-  ],
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.m?js$/i,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              [
-                '@babel/preset-env',
-                {
-                  useBuiltIns: 'entry',
-                  corejs: '3.20',
-                },
+module.exports = (env, argv) => {
+  const mode = (argv.mode === 'development') ? 'development' : 'production';
+
+  // Base configuration
+  const config = {
+    entry: './src/index.js',
+    mode,
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './src/template.ejs',
+        title: 'Task It Up',
+      }),
+    ],
+    output: {
+      filename: 'main.js',
+      path: path.resolve(__dirname, 'dist'),
+      clean: true,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.m?js$/i,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                [
+                  '@babel/preset-env',
+                  {
+                    useBuiltIns: 'entry',
+                    corejs: '3.20',
+                  },
+                ],
               ],
-            ],
+            },
           },
         },
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        },
+      ],
+    },
+  };
+
+  // Add development options if needed
+  if (mode === 'development') {
+    Object.assign(config, {
+      devtool: 'inline-source-map',
+      devServer: {
+        static: './dist',
       },
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
-    ],
-  },
+    });
+  }
+
+  return config;
 };
