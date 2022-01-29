30f6bd77c7b5c3ad148a2dd09404fceef5a3af52
Greg Kikola <gkikola@gmail.com>
2022-01-28 22:53:03 -0500

    Add form elements to settings modal

--- src/modules/modals/settingsModal.js
+++ src/modules/modals/settingsModal.js
@@ -3,6 +3,9 @@
  * @module settingsModal
  */
 
+import Settings from '../settings';
+import { createFormControl } from '../utility';
+
 /**
  * Object holding private members for the
  * [SettingsModal]{@link module:settingsModal~SettingsModal} class.
@@ -40,6 +43,13 @@ class SettingsModal {
    *   when the user cancels the modal.
    */
 
+  /**
+   * Initialize the modal.
+   * @param {module:settings~Settings} settings The app settings being
+   *   modified.
+   * @param {module:settingsModal~SettingsModal~options} [options={}] Holds
+   *   configuration options for the modal.
+   */
   constructor(settings, options = {}) {
     const privates = {
       settings,
@@ -63,9 +73,86 @@ class SettingsModal {
    * Temporary until content is added.
    */
   addContent(parent) {
-    const message = document.createElement('p');
-    message.textContent = 'TODO: Add Settings';
-    parent.appendChild(message);
+    const dateFormats = [
+      'local',
+      'iso',
+      'month-day-year',
+      'day-month-year',
+      'year-month-day',
+    ].map((value) => {
+      const format = Settings.lookupDateFormat(value).visual;
+      let description;
+      switch (value) {
+        case 'local':
+          description = 'Local Format';
+          break;
+        case 'iso':
+          description = 'ISO';
+          break;
+        case 'month-day-year':
+          description = 'Month/Day/Year';
+          break;
+        case 'day-month-year':
+          description = 'Day/Month/Year';
+          break;
+        case 'year-month-day':
+          description = 'Year/Month/Day';
+          break;
+        default:
+          break;
+      }
+
+      return { value, label: `${description}: ${format}` };
+    });
+    parent.appendChild(createFormControl({
+      type: 'select',
+      id: 'settings-date-format',
+      name: 'settings-date-format',
+      classList: ['form-select'],
+      label: { value: 'Date Format', classList: ['form-input-label'] },
+      container: { classList: ['form-input-container'] },
+      menuItems: dateFormats,
+    }));
+
+    const container = document.createElement('div');
+    container.classList.add('form-input-container');
+
+    let label = document.createElement('div');
+    label.classList.add('form-input-label');
+    label.textContent = 'Additional Options';
+    container.appendChild(label);
+
+    const optionContainer = document.createElement('div');
+    optionContainer.classList.add('form-input-item-container');
+    optionContainer.appendChild(createFormControl({
+      type: 'checkbox',
+      id: 'settings-delete-old-tasks',
+      name: 'settings-delete-tasks',
+      value: 'delete-old',
+    }));
+    label = document.createElement('label');
+    label.classList.add('form-input-label-inline');
+    label.htmlFor = 'settings-delete-old-tasks';
+    label.textContent = 'Delete completed tasks after ';
+    optionContainer.appendChild(label);
+
+    optionContainer.appendChild(createFormControl({
+      type: 'number',
+      id: 'settings-delete-task-day-count',
+      name: 'settings-delete-task-day-count',
+      value: '1',
+      classList: ['form-input-inline', 'form-input-count'],
+      required: true,
+      min: 0,
+    }));
+    label = document.createElement('label');
+    label.classList.add('form-input-label-inline');
+    label.htmlFor = 'settings-delete-task-day-count';
+    label.textContent = ' days';
+    optionContainer.appendChild(label);
+
+    container.appendChild(optionContainer);
+    parent.appendChild(container);
   }
 
   confirm() {
