5bfc3fdab98168f6be87ea8eb44518252d46befc
Greg Kikola <gkikola@gmail.com>
2022-02-15 20:19:58 -0500

    Add arrayToCsvRecord utility function

--- src/modules/utility.js
+++ src/modules/utility.js
@@ -376,6 +376,48 @@ function createToggleButton(label, options = {}) {
 }
 
 /**
+ * Convert an array of strings to comma-separated values (CSV format, as
+ * described by the
+ * [RFC 4180]{@link https://datatracker.ietf.org/doc/html/rfc4180}
+ * specification). This function will create one CSV record, which can then be
+ * combined with other records to form a complete CSV file.
+ * @param {string[]} data An array of strings specifying the values for the
+ *   fields in the record.
+ * @param {Object} [options={}] An object specifying options for the
+ *   conversion.
+ * @param {string} [options.newlineSequence] The character sequence to use for
+ *   newlines. If provided, newline characters within each field will be
+ *   replaced with this sequence.
+ * @returns {string} The record in CSV format. The returned string is not
+ *   terminated by a newline, but may contain quoted newlines within the field
+ *   data.
+ */
+function arrayToCsvRecord(data, options = {}) {
+  const newlineSequence = options.newlineSequence || '\n';
+
+  const fields = [];
+  data.forEach((value) => {
+    let needQuotes = false;
+    const result = value.replace(/,|"|\r\n|\n|\r/g, (match) => {
+      needQuotes = true;
+      switch (match) {
+        case '"':
+          return '""';
+        case '\r\n':
+        case '\n':
+          return newlineSequence;
+        default:
+          return match;
+      }
+    });
+
+    fields.push(needQuotes ? `"${result}"` : result);
+  });
+
+  return fields.join(',');
+}
+
+/**
  * Find a value in an array belonging to a Map having array values. The first
  * value in the appropriate array for which the predicate returns true is
  * returned.
@@ -689,6 +731,7 @@ export {
   createFormControl,
   createIconButton,
   createToggleButton,
+  arrayToCsvRecord,
   findInMapArray,
   getDateFormat,
   formatDate,
