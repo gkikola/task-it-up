9ae7163df0bed4c5c40edf59839944356754cddd
Greg Kikola <gkikola@gmail.com>
2022-03-27 20:41:03 -0400

    Add isUuidValid and validateValue utility funcs

--- src/modules/utility/data.js
+++ src/modules/utility/data.js
@@ -3,6 +3,13 @@
  * @module data
  */
 
+import {
+  validate as validateUuid,
+  version as uuidVersion,
+} from 'uuid';
+
+import { isDateValid, parseIsoDateTime } from './dates';
+
 /**
  * Add a value to an array belonging to a Map having array values. If the given
  * key does not exist in the map, then a new array will be inserted at that
@@ -104,10 +111,238 @@ function getJsonType(value) {
   }
 }
 
+/**
+ * Determine whether a string is a valid version 4 UUID.
+ * @param {string} id The string to test.
+ * @returns {boolean} True if the UUID is valid, and false otherwise.
+ */
+function isUuidValid(id) {
+  return typeof id === 'string' && validateUuid(id) && uuidVersion(id) === 4;
+}
+
+/**
+ * A callback function that will be invoked when a value passes validation.
+ * @callback module:data~validationSuccess
+ * @param {*} value The value that passed validation. If the allowConversion
+ *   option was set, then this will be the converted value.
+ * @param {Object} options An object specifying additional information.
+ * @param {string} [options.valueName] The name for the data field that the
+ *   value corresponds to.
+ */
+
+/**
+ * A callback function that will be invoked when a value fails validation.
+ * @callback module:data~validationError
+ * @param {string} errorType A string specifying the type of error that
+ *   occurred: 'bad-type' indicates a type error, 'unknown-value' indicates an
+ *   unrecognized value, 'not-integer' indicates a value that is not an
+ *   integer, 'too-low' indicates a value below the minimum, 'too-high'
+ *   indicates a value above the maximum, 'bad-date' indicates an invalid date,
+ *   'bad-id' indicates an invalid UUID, and 'failed-predicate' indicates that
+ *   the value was rejected by the custom predicate function.
+ * @param {*} value The value that failed validation.
+ * @param {Object} options An object specifying the criteria that were used to
+ *   validate the value.
+ * @param {string} [options.valueName] The name for the data field that the
+ *   value corresponds to.
+ * @param {string} [options.expectedType] The expected type of the value.
+ * @param {boolean} [options.allowConversion] Indicates whether conversion from
+ *   string was allowed.
+ * @param {string[]} [options.expectedValues] An array of acceptable strings
+ *   that the value should have matched.
+ * @param {boolean} [options.requireUuid] Indicates whether a UUID was
+ *   required.
+ * @param {boolean} [options.requireInteger] Indicates whether an integer was
+ *   required.
+ * @param {number} [options.min] The minimum acceptable value.
+ * @param {number} [options.max] The maximum acceptable value.
+ * @param {Function} [options.customPredicate] The custom predicate function
+ *   that was used for validation.
+ */
+
+/**
+ * An object specifying options for validating a value.
+ * @typedef {Object} module:data~validationOptions
+ * @property {string} [valueName] An optional name for the data field that the
+ *   value is read from or being assigned to. This is passed to the callback
+ *   functions but is otherwise ignored.
+ * @property {string} [expectedType] The type that the value should have. This
+ *   can be one of 'object', 'array', 'string', 'number', 'boolean', 'null', or
+ *   'date'.
+ * @property {boolean} [allowConversion=false] This property is used in
+ *   conjunction with expectedType. If this is set to true, then the value is
+ *   allowed to have string type as long as it can be converted to the expected
+ *   type. Objects and arrays cannot be converted from string.
+ * @property {string[]} [expectedValues] For string values, this property
+ *   specifies an optional list of acceptable values. Values that do match an
+ *   item in the given array will fail validation.
+ * @property {boolean} [requireUuid=false] For string values, if this property
+ *   is set to true, then the string must contain a UUID.
+ * @property {boolean} [requireInteger=false] For values of number type, if
+ *   this property is set to true, then the number must be an integer.
+ * @property {number} [min] For numeric values, this property specifies the
+ *   minimum acceptable value.
+ * @property {number} [max] For numeric values, this property specifies the
+ *   maximum acceptable value.
+ * @property {Function} [customPredicate] A predicate function that can perform
+ *   custom validation. If the value passes all other constraints, then it is
+ *   passed to this function. The function should return true or false to
+ *   indicate whether the value passes validation.
+ * @property {module:data~validationSuccess} [successCallback] A callback
+ *   function that will be invoked if the value passes validation.
+ * @property {module:data~validationError} [errorCallback] A callback
+ *   function that will be invoked if the value fails validation.
+ */
+
+/**
+ * Validate a value according to a set of criteria.
+ * @param {*} value The value to check.
+ * @param {module:data~validationOptions} [options={}] An object specifying
+ *   validation options.
+ * @returns {boolean} True if the value passes validation, and false otherwise.
+ */
+function validateValue(value, options = {}) {
+  const fail = (errorType) => {
+    options.errorCallback?.(errorType, value, {
+      valueName: options.valueName ?? null,
+      expectedType: options.expectedType ?? null,
+      allowConversion: options.allowConversion || false,
+      expectedValues: options.expectedValues || null,
+      requireUuid: options.requireUuid || false,
+      requireInteger: options.requireInteger || false,
+      min: options.min ?? null,
+      max: options.max ?? null,
+      customPredicate: options.customPredicate || null,
+    });
+  };
+
+  const allowConversion = options.allowConversion || false;
+  let convertedValue = value;
+  if (options.expectedType) {
+    if (options.expectedType !== 'null' && value == null) {
+      fail('bad-type');
+      return false;
+    }
+
+    switch (options.expectedType) {
+      case 'string':
+        if (typeof value !== 'string') {
+          fail('bad-type');
+          return false;
+        }
+        break;
+      case 'number':
+        if (typeof value !== 'number') {
+          if (!allowConversion || typeof value !== 'string') {
+            fail('bad-type');
+            return false;
+          }
+          convertedValue = Number(value);
+        }
+
+        if (!Number.isFinite(convertedValue)) {
+          fail('bad-type');
+          return false;
+        }
+        break;
+      case 'boolean':
+        if (typeof value !== 'boolean') {
+          if (!allowConversion || typeof value !== 'string') {
+            fail('bad-type');
+            return false;
+          }
+
+          switch (value.toLowerCase()) {
+            case 'true':
+              convertedValue = true;
+              break;
+            case 'false':
+              convertedValue = false;
+              break;
+            default:
+              fail('bad-type');
+              return false;
+          }
+        }
+        break;
+      case 'null':
+        if (value != null) {
+          fail('bad-type');
+          return false;
+        }
+        break;
+      case 'date':
+        if (!(value instanceof Date)) {
+          if (!allowConversion || typeof value !== 'string') {
+            fail('bad-type');
+            return false;
+          }
+          convertedValue = parseIsoDateTime(value);
+        }
+
+        if (!isDateValid(convertedValue)) {
+          fail('bad-date');
+          return false;
+        }
+        break;
+      default:
+        if (getJsonType(value) !== options.expectedType) {
+          fail('bad-type');
+          return false;
+        }
+        break;
+    }
+  }
+
+  if (typeof convertedValue === 'string') {
+    if (options.expectedValues
+      && !options.expectedValues.includes(convertedValue)) {
+      fail('unknown-value');
+      return false;
+    }
+
+    if (options.requireUuid && !isUuidValid(convertedValue)) {
+      fail('bad-id');
+      return false;
+    }
+  }
+
+  if (typeof convertedValue === 'number') {
+    if (options.requireInteger && !Number.isInteger(convertedValue)) {
+      fail('not-integer');
+      return false;
+    }
+
+    if (options.min != null && convertedValue < options.min) {
+      fail('too-low');
+      return false;
+    }
+
+    if (options.max != null && convertedValue > options.max) {
+      fail('too-high');
+      return false;
+    }
+  }
+
+  if (options.customPredicate) {
+    if (!options.customPredicate(convertedValue)) {
+      fail('failed-predicate');
+      return false;
+    }
+  }
+
+  options.successCallback?.(convertedValue, {
+    valueName: options.valueName ?? null,
+  });
+  return true;
+}
+
 export {
   addToMapArray,
   findInMapArray,
   getJsonType,
+  isUuidValid,
   removeFromMapArray,
   removeFromMapArrayBy,
+  validateValue,
 };
